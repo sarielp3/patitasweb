ca3d98175300539c292957f996e09200340361aa
sarielp3 <86993380+sarielp3@users.noreply.github.com>
2022-01-07 23:54:40 -0700

    Update server.js

--- src/app/server.js
+++ src/app/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(__dirname+'/dist/patitasweb/));
+app.use(express.static(__dirname+'/dist/patitasweb/'));
 
 app.get('/*', function(req,res) =>{
     res.sendFile(path.join(__dirname+'/dist/patitasweb/index.html'));
